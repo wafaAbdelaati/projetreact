24ea5e6c93536ebc941703755f918c4e537ea6bc
wafa ABDELAATI <wafa.abdelaati@sesame.com.tn>
2021-01-20 13:04:50 +0100

    c bon

--- src/Home.js
+++ src/Home.js
@@ -5,11 +5,15 @@ class Home extends Component{
     render() {
         return (
 
-            <div >
-                <h2 > HELLO </h2>
-                <p> Cras facilisis urna ornare ex volutpat, et convallis erat elementum.Ut aliquam, ipsum vitae gravida suscipit, metus dui bibendum est, eget rhoncus nibh metus nec massa.Maecenas hendrerit laoreet augue nec molestie.Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. </p>
-                <p> Duis a turpis sed lacus dapibus elementum sed eu lectus. </p>  
-                 </div>
+            <div class="home">
+                <h2 > WELCOME TO PAGE HOME</h2>
+                   <div class="parag">
+                        <p> Le lorem ipsum (également appelé faux-texte, lipsum, ou bolo bolo1) est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès qu'il est prêt ou que la mise en page est achevée.Généralement, on utilise un texte en faux latin (le texte ne veut rien dire, il a été modifié), le Lorem ipsum ou Lipsum. L'avantage du latin est que l'opérateur sait au premier coup d'œil que la page contenant ces lignes n'est pas valide et que l'attention du lecteur n'est pas dérangée par le contenu, lui permettant de demeurer concentré sur le seul aspect graphique. </p>  
+                        <p>Le lorem ipsum (également appelé faux-texte, lipsum, ou bolo bolo1) est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès qu'il est prêt ou que la mise en page est achevée.Généralement, on utilise un texte en faux latin (le texte ne veut rien dire, il a été modifié), le Lorem ipsum ou Lipsum. L'avantage du latin est que l'opérateur sait au premier coup d'œil que la page contenant ces lignes n'est pas valide et que l'attention du lecteur n'est pas dérangée par le contenu, lui permettant de demeurer concentré sur le seul aspect graphique. </p>  
+                        <p> Cras facilisis urna ornare ex volutpat, et convallis erat elementum.Ut aliquam, ipsum vitae gravida suscipit, metus dui bibendum est, eget rhoncus nibh metus nec massa.Maecenas hendrerit laoreet augue nec molestie.Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. </p>
+                        <p> Duis a turpis sed lacus dapibus elementum sed eu lectus. </p>  
+                    </div>
+            </div>
         );
     }
 }
